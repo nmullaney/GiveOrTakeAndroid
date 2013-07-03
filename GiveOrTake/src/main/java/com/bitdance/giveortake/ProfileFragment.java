ebbf31c62b0e5427f9bdb1bb0eb655aa7ff4ad84
nmullaney <nora.mullaney@gmail.com>
2013-07-03 15:02:26 -0700

    First attempt at building Profile. This version is using TableLayout, but isn't a proper list.

--- GiveOrTake/src/main/java/com/bitdance/giveortake/ProfileFragment.java
+++ GiveOrTake/src/main/java/com/bitdance/giveortake/ProfileFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by nora on 6/19/13.
@@ -19,6 +20,11 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        ActiveUser activeUser = ActiveUser.getInstance();
+        TextView usernameView = (TextView)v.findViewById(R.id.profile_username);
+        usernameView.setText(activeUser.getUserName());
+        TextView emailView = (TextView)v.findViewById(R.id.profile_email);
+        emailView.setText(activeUser.getEmail());
         return v;
     }
 
